339dcde90e2ff254f0bb73ddb616c80f5a3f0983
Adrian Ibañez <hello@adrianibanez.info>
2025-03-28 10:48:12 +0100

    sigmajs POC

--- mix.exs
+++ mix.exs
@@ -11,7 +11,8 @@ defmodule IrohEx.MixProject do
       version: @version,
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -26,9 +27,30 @@ defmodule IrohEx.MixProject do
   defp deps do
     [
       {:rustler, "~> 0.36.1"},
-      {:rustler_precompiled, "~> 0.7"}
+      {:rustler_precompiled, "~> 0.7"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      description: "Elixir binding of iroh p2p lib",
+      links: %{
+        GitHub: @source_url,
+        #LiveBook:
+        #"https://livebook.dev/run/?url=#{@source_url}/blob/main/livebooks/ble_demo.livemd"
+      },
+      files: [
+        "lib",
+        "native/iroh_ex/.cargo",
+        "native/iroh_ex/src",
+        "native/iroh_ex/Cargo*",
+        "checksum-*.exs",
+        "mix.exs"
+      ]
     ]
   end
 end
