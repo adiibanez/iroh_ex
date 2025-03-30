90d1540e9883c734643bc08f4caa133a4fb7382b
Adrian Ibañez <hello@adrianibanez.info>
2025-03-30 16:48:41 +0200

    updates to ui

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule IrohEx.MixProject do
     [
       app: :iroh_ex,
       version: @version,
-      elixir: "~> 1.18",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package()
