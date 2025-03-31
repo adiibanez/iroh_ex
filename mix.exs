7c07350318f771508ab96bb8d8f4be75cb9999b4
Adrian Ibañez <hello@adrianibanez.info>
2025-03-31 23:39:26 +0200

    update version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule IrohEx.MixProject do
   use Mix.Project
 
-  @version "0.0.3"
+  @version "0.0.4"
   @source_url "https://github.com/adiibanez/iroh_ex"
   @dev? String.ends_with?(@version, "-dev")
 
