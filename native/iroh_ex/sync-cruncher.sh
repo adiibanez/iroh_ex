1c0c2cb2a654738b3ca7eba9f5ed6c5d36eb2d73
Adrian Ibañez <hello@adrianibanez.info>
2025-04-13 17:34:46 +0200

    changes before actor refactoring

--- native/iroh_ex/sync-cruncher.sh
+++
@@ -1,5 +0,0 @@
-rsync --progress -rte "ssh -p 22" \
-  --delete \
-  --exclude .vscode --exclude .cargo --exclude target \
-  --exclude .DS_Store \
-  ./ 192.168.1.195:projects/2025_sensocto/checkouts/iroh_ex/native/iroh_ex
